e13b5b1e1592fe1485f891905a4578c38e0e84db
Niek Vincent <apocalypsjenl@gmail.com>
2019-05-09 20:13:34 +0200

    Initial Push

---
+++ src/main/java/nl/apocalypsje/bedrock/FormLib.java
@@ -0,0 +1,11 @@
+package nl.apocalypsje.bedrock;
+
+import cn.nukkit.plugin.PluginBase;
+
+public class FormLib extends PluginBase {
+
+    @Override
+    public void onEnable() {
+        
+    }
+}
